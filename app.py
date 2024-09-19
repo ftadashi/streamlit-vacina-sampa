ab43213939e75ac4b5438d37f131f06fc638e68e
Fernando Tadashi Akimoto 🇧🇷 <akimoto@adobe.com>
2024-09-19 16:00:06 -0300

    Refactoring: split elements across tabs, commented remove columns feature (issues on list view).

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from utils.api import api_call
 from utils.data import convert_data, filter_data
 from utils.log import info
-from utils.columns import CRS, STATUS_FILA, STATUS_FILA, COUNTS, INDEX, VACCINES_COLUMNS
+from utils.columns import CRS, STATUS_FILA, STATUS_FILA, COUNTS, INDEX, VACCINES_COLUMNS, EQUIPAMENTO, ENDERECO, CRS, DATA_HORA, STATUS_FILA
 
 # Global configurations
 CACHE_TTL = 300
@@ -36,10 +36,10 @@ with st.sidebar.expander('Distrito'):
     district_list = list(data['distrito'].unique())
     district = st.multiselect('Selecione o distrito', district_list, district_list)
 
-st.sidebar.title(':material/view_column: Colunas')
-with st.sidebar.expander('Colunas'):
-    columns_list = list(data.columns)
-    columns = st.multiselect('Selecione as colunas', columns_list, columns_list)
+# st.sidebar.title(':material/view_column: Colunas')
+# with st.sidebar.expander('Colunas'):
+#     columns_list = list(data.columns)
+#     columns = st.multiselect('Selecione as colunas', columns_list, columns_list)
 
 # Filtered data
 filtered_data = filter_data(data, 
@@ -47,11 +47,16 @@ filtered_data = filter_data(data,
                             district=district, 
                             region=region,
                             queue_status=queue_status)
-filtered_data = filtered_data[columns] # remove columns from filter
+#filtered_data = filtered_data[columns] # remove columns from filter
 
-# Metric section (data can be filtered)
-with st.expander('Métricas gerais', icon=':material/score:', expanded=True):
-    
+# Tabs
+tab1, tab2, tab3 = st.tabs([
+    ':material/home_health: Unidades',
+    ':material/score: Métricas gerais',
+    ':material/database: Dados'
+    ])
+
+with tab2:
     col1, col2, col3 = st.columns([2, 3, 3])
 
     # last update present on data
@@ -65,12 +70,12 @@ with st.expander('Métricas gerais', icon=':material/score:', expanded=True):
         counts_status_fila = filtered_data[STATUS_FILA].value_counts().reset_index(name=COUNTS)
         counts_status_fila.sort_values(by=[COUNTS], ascending=False)
         st.bar_chart(counts_status_fila, 
-                     x=STATUS_FILA, 
-                     y=COUNTS, 
-                     x_label='Quantidade',
-                     y_label='Status da fila',
-                     color=STATUS_FILA,
-                     horizontal=True)
+                    x=STATUS_FILA, 
+                    y=COUNTS, 
+                    x_label='Quantidade',
+                    y_label='Status da fila',
+                    color=STATUS_FILA,
+                    horizontal=True)
 
     # Chart: count vaccines available (> 0)
     with col3:
@@ -86,7 +91,20 @@ with st.expander('Métricas gerais', icon=':material/score:', expanded=True):
                 horizontal=True)
 
 # Table data filtered
-with st.container():
-    st.subheader(':material/database: Dados', divider='red')
+with tab3:
     st.dataframe(filtered_data, use_container_width=True)
     st.markdown(f'A tabela possui **:red[{filtered_data.shape[0]}]** linhas e **:red[{filtered_data.shape[1]}]** colunas')
+
+# Cards list
+with tab1:
+    for index, row in filtered_data.iterrows():
+        with st.expander(row[EQUIPAMENTO], expanded=True, icon=':material/medical_services:'):
+            st.markdown(f':material/explore: {row[ENDERECO]} - {row[CRS]}')
+            st.markdown(f':material/hourglass: **{row[STATUS_FILA]}**')
+
+            st.markdown(f'##### :material/syringe: Vacinas disponíveis (última atualização às {row[DATA_HORA].strftime('%d/%m/%Y %H:%M:%S')})')
+            for vaccine in VACCINES_COLUMNS:
+                if not row[vaccine]:
+                    row = row.drop([vaccine])
+                else:
+                    st.markdown(f':material/check_small: {vaccine}')
